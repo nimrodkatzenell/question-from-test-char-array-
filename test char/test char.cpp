d6c16315a3b60a1fd020e9bfe0819a8a1bc038d7
noaka <noaka@STEVE>
2023-01-19 13:07:37 +0200

    Add project files.

---
+++ test char/test char.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <string.h>
+using namespace std;
+int shortest(char* s);
+int main()
+{
+	char s[] = "ghuyhhygag";
+	cout << shortest(s);
+
+}
+int shortest(char* s)
+{
+	int num = 0;
+	int counter = 0;
+	int temp;
+	for (int i = 0; i < strlen(s); i++)
+	{
+		for (int j = i; j < strlen(s); j++)
+		{
+			if (s[i] == s[j] && i != j)
+			{
+				num++;
+			}
+		}
+	}
+	int* A = new int[num];
+	if (!A)
+	{
+		cout << "out of memory";
+		return 0;
+	}
+	for (int i = 0; i < strlen(s); i++)
+	{
+		for (int j = i; j < strlen(s); j++)
+		{
+			if (s[i] == s[j] && i != j)
+			{
+				A[counter] = j - i-1;
+				counter++;
+			}
+		}
+	}
+	for (int i = 0; i < num; i++)
+	{
+		for (int j = i; j < num; j++)
+		{
+			if (A[i] > A[j])
+			{
+				temp = A[i];
+				A[i] = A[j];
+				A[j] = temp;
+			}
+		}
+	}
+	temp = A[0];
+	delete[] A;
+	return temp;
+}
